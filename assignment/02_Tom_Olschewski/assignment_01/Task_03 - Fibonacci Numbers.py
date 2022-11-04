0b06893b9fd14b011d450bd87a18adee384e07d4
Tom Olschewski <tom.olli@icloud.com>
2022-11-04 17:40:02 +0100

    Fixed issue on Task_03

--- assignment/02_Tom_Olschewski/assignment_01/Task_03 - Fibonacci Numbers.py
+++ assignment/02_Tom_Olschewski/assignment_01/Task_03 - Fibonacci Numbers.py
@@ -2,11 +2,11 @@ n = int(input("Please enter your desired Number : "))
 fib0 = 0
 fib1 = 1
 
-count = 0
-
-while(n>count):
+while(n>fib1 and n>fib0):
     print(fib0)
     fib1 = fib1 +fib0
-    print(fib1)
-    fib0 = fib1 + fib0
-    count = count + 1
+
+    if n>fib1:
+        print(fib1)
+        fib0 = fib1 + fib0
+
