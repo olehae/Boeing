9d4019ab2c353eb1424d395ae5b02e56936da5f6
olehae <das.typ.2003@gmail.com>
2023-01-10 22:40:33 +0100

    add the ability to save booked seats to db and display them accordingly in booking.html

--- project/boeing/booking/views.py
+++ project/boeing/booking/views.py
@@ -10,12 +10,37 @@ def index(request):
     cursor = connection.cursor()
 
     # get data from chartIn3 table
-    seats = cursor.execute("SELECT Row, Seat FROM chartIn3").fetchall()
+    seats = cursor.execute("SELECT Row, Seat, Occupied FROM {}".format("chartIn3")).fetchall()
     rows = seats[len(seats)-1][0]
 
-    # get the names for one row of seats
-    seat_set = [item[1] for item in seats[:int(len(seats)/rows)]]
+    # Get list of Occupied Column
+    occupied = [item[2] for item in seats]
+    # Seat Character and Row Number are added to one string
+    seats = [str(item[0])+str(item[1]) for item in seats]
+    # this is needed to format the html file if a seat is occupied
+    occupied_looking_seats = ["-"+seat+"-" for seat in seats]
+
+    # the left seats (Character A) are only needed to format the grid of buttons (<br> before left seat)
+    left_seat = seats[::int(len(seats) / rows)]
+    left_seat += ["-"+i+"-" for i in left_seat]
+
+    # Mark the occupied seats as "-seat-" example: "0A" is not occupied but "-0A-" is
+    for i, status in enumerate(occupied):
+        if status:
+            seats[i] = "-"+seats[i]+"-"
+
+    # get a list of all the clicked buttons after the submit button is hit
+    clicked = request.POST.getlist("check")
+    for seat in clicked:
+        # split the strings into row and seat again in order to update their status in the database
+        booked_row = int(''.join(i for i in seat if i.isdigit()))
+        booked_seat = ''.join(i for i in seat if not i.isdigit())
+        cursor.execute("""UPDATE {} SET Occupied = TRUE WHERE Row = ? AND Seat = ?"""
+                       .format("chartIn3"), (booked_row, booked_seat))
+        # commit the changes to the database
+        connection.commit()
     connection.close()
 
-    # gives values range(rows) and seat_set to booking.html
-    return render(request, "booking.html", {"rows": range(rows), "seat_set": seat_set})
+    values = {"seats": seats, "left_seat": left_seat, "occupied": occupied_looking_seats}
+    # gives values defined above to booking.html file
+    return render(request, "booking.html", values)
