2b28edf355e1d28b81b3b9aa3b6f5dcd7dedb4ec
tomschewski <109431200+tomschewski@users.noreply.github.com>
2023-01-30 19:10:24 +0100

    Update urls.py

--- project/boeing/accounts/urls.py
+++ project/boeing/accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("signup/", views.sign_up_view, name="signup"),  # show builtin SignUpView in accounts/signup/ path
     path("emailconfirmation/", views.email_confirmation_view, name="emailconfirmation"),
     path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout")
+    path("logout/", views.logout_view, name="logout"),
+    path("deleteaccount/", views.delete_account_view, name="deleteaccount")
 ]
